6a5c3bf9a2eecd65fd9969d4ac5ff8b9b7aec1c8
JordanoBaluz <jordanolb95@hotmail.com>
2022-09-29 21:11:17 -0300

    importado biblioteca de icones, finalizado sidebar com importação da imagem de avatar e propriedades css da sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -8,11 +9,13 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
             />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/jordanobaluz.png" />
                 <strong>Jordano Baluz</strong>
                 <span>Web Developer</span>
             </div>
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
