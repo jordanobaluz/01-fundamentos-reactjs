58bfa8931910edfce6e2538b6b0b8098818d0121
JordanoBaluz <jordanolb95@hotmail.com>
2022-10-04 16:27:55 -0300

    refatorado e criado o componente avatar que irá exibir as fotos de avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,15 +1,16 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
+import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img className={styles.avatar} src="https://github.com/jordanobaluz.png" />
+            <Avatar hasBorder={false} src="https://github.com/AugustoCalaca.png" />
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
                         <div className={styles.authorAndTime}>
-                            <strong>Jordano Baluz</strong>
+                            <strong>Augusto Calaca</strong>
                             <time
                                 title='29 de Setembro às 21:20'
                                 dateTime='2022-09-29 21:20'
