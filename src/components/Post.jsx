b5a53630a285b6e5c44042e0ab2e5240f69466fe
JordanoBaluz <jordanolb95@hotmail.com>
2022-10-06 20:42:15 -0300

    refatorado o post para trabalhar o campo de comentários, trabalhado o estado do componente e agora ele adiciona novos comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,8 +4,11 @@ import styles from './Post.module.css'
 
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBr from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
 
 export function Post({ author, publishedAt, content }) {
+    const [comments, setComments] = useState([1, 2])
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBr
     })
@@ -17,6 +20,12 @@ export function Post({ author, publishedAt, content }) {
             addSuffix: true
         }
     )
+
+    function handleCreateNewComment() {
+        event.preventDefault()
+        setComments([...comments, comments.length + 1])
+        console.log("oi")
+    }
 
     return (
         <article className={styles.post}>
@@ -45,7 +54,7 @@ export function Post({ author, publishedAt, content }) {
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
                 <textarea
                     placeholder='Deixe um comentário'
@@ -55,9 +64,11 @@ export function Post({ author, publishedAt, content }) {
                 </footer>
             </form>
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return (
+                        <Comment />
+                    )
+                })}
             </div>
         </article>
     )
